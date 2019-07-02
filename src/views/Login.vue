70d5f6fc1970cb79d4599d54bd0eb8323b9f41f5
Luther521 <784939830@qq.com>
2019-07-02 16:27:02 +0800

    修改登录页面提示语

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -34,10 +34,10 @@
                 },
                 rules: {
                     username: [
-                        { required: true, message: '请输入用户名', trigger: 'blur' }
+                        { required: true, message: '请输入登录账号', trigger: 'blur' }
                     ],
                     password: [
-                        { required: true, message: '请输入密码', trigger: 'blur' }
+                        { required: true, message: '请输入登录密码输入密码', trigger: 'blur' }
                     ]
                 }
             }
